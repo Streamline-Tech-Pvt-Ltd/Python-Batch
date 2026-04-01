11994e41f73a488eef4ae3f34f276124b153d633
rahul-pawar1155 <rbpawar870@gmail.com>
2026-04-01 14:00:22 +0530

    Updated

---
+++ Python Notes/for loop.py
@@ -0,0 +1,30 @@
+#  For Loop :-
+
+            #   used to repeat a block of code multiple times
+
+
+   # example 1. :  without range
+
+
+          # numbers = [10, 20, 30, 40]
+          # for n in numbers:
+          # print(n)
+
+    # example 2. : with range
+             
+        # a.      # for n in range(10):
+                  #     print (n)
+                    
+                  # output : 0---9
+
+
+
+        # b.       # for i in range(1,6):
+                   # print(i)
+
+                    # output : 1---5
+
+
+        # c.       # for i in range(1,101,2):
+                   #     print (i)
+
