81738fd17857b5c20e29f02daba3097745d07432
rahul-pawar1155 <rbpawar870@gmail.com>
2026-04-08 19:53:55 +0530

    python Notes

---
+++ Rahul_Pawar/core python/conditional statement.py
@@ -0,0 +1,81 @@
+# onditional Statements
+
+#  if - elif - else
+
+# if : if is use to single condition
+
+# number = [1,2,3,4]
+
+# if 7 not in number:
+#     print("number is not in a string")
+
+
+
+#  if - else : for the 2 condition
+
+
+# age = int(input("Enter Your age :- "))
+
+# if age > 18 : 
+#     print("you can apply for the company")
+    
+# else:
+#     print("you cant apply for the company")
+    
+    
+
+#if - else -elif   :(for more then 2 condition)
+
+# marks = int(input("Enter Your Marks :- "))
+
+# if marks > 75 :
+#     print("Grade A")
+    
+# elif marks > 50:
+#     print("Grade B") 
+    
+# elif marks > 35:
+#     print("Grade C")
+    
+# else:
+#     print("Fail")
+    
+
+
+
+
+#  transfer  statements : change the flow of control in a program
+
+# a)break
+# b)continue
+# c)pass
+
+
+# a) break : use for the stop loop
+
+# num = [11,22,33,44,55]
+
+# for i in num:
+#     if i == 33:
+#         break
+#     print(i)
+
+
+# b) continue : skip the current iteration
+
+# num = [11,22,33,44,55]
+
+# for i in num:
+#     if i == 33:
+#         continue
+#     print(i)
+
+
+# c) pass  : that mean does nothing
+
+# num = [11,22,33,44,55]
+
+# for i in num:
+#     if i == 33:
+#         pass
+#     print(i)
---
+++ Rahul_Pawar/core python/lambda.py
@@ -0,0 +1,22 @@
+# Lambda :-
+
+    #     it is small anonymous function
+    #     use for short operations
+
+
+
+   #    example:
+    
+   #    1.          # x = lambda a: a + 10
+                    # print (x(5))                   (a is parameter and 5 is arg)
+       
+    #   2.    
+
+                # def add(a, b):
+                #     return a + b
+                # # print(add(4,5))
+                
+                   # OR 
+
+               # lambda a, b: a + b
+               # print(add(4,5))
