1b8edba124bb69dedce08a93a6a6df8372b94914
Urmilasaid07 <urmilasaid07@gmail.com>
2026-06-14 11:58:16 +0530

    Added data types notes

--- Urmila_Said/Python/Data_type.py
+++ Urmila_Said/Python/Data_type.py
@@ -35,20 +35,13 @@ print(id(num1))
 num2 = 103
 print(id(num2))
 
-#Task
+#Task 1
 
 print("Product Information")
 product_name = "Powder"
-print("Product Name:", product_name)
-
 product_id = 102
-print("Product Name:",product_id)
-
 product_category = "makeup"
-print("Product Category:",product_category)
-
 product_prize = 10
-print("Product Prize:",product_prize)
 
 print(f"Product Name:{product_name},\nProduct ID: {product_id},\nProduct Category:{product_category},\nProduct Price: {product_prize}")
 
@@ -64,6 +57,22 @@ print(type(product_category))
 product_prize = 10
 print(type(product_prize))
 
+# Task 2
+
+print("Product Information")
+product_name = "Rice"
+product_id = 10
+product_category = "Food"
+product_prize = 100
+print("Product Name:", product_name,
+      "\nProduct Id:" , product_id,
+      "\nProduct Prize:" , product_prize,
+      "\nProduct Category:" , product_category)
+
+print(type(product_name))
+print(type(product_id))
+print(type(product_category))
+print(type(product_prize))
 
 
 
