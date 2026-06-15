2d7e88d9bac71f0ac8d33e74cf6bcf286e98e982
Urmilasaid07 <urmilasaid07@gmail.com>
2026-06-15 19:01:23 +0530

    Notes Uploaded

---
+++ Urmila_Said/Python/Indexing.py
@@ -0,0 +1,24 @@
+# Indexing in python 
+# Indexing means accessing individual elements of a sequence (like a string, list, or tuple) using their position number (index).
+# Indexing alwaya Start from 0.
+# Positive indexing start from the left side.
+# Negative Indexing start from the right end.
+ 
+# EX:
+List = [ 10,20,30,40,50,60]
+
+# positive
+print(List[1])
+print(List[3])
+print(List[5])
+
+# Negative
+print(List[-1])
+print(List[-6])
+print(List[-3])
+print(List[-5])
+
+
+
+
+
