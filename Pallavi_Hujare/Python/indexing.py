7628ffe5bafa3ff8734fde390b38b55c5acfad7e
pallavihujare123-cell <pallavihujare123@gmail.com>
2026-06-15 23:26:34 +0530

    Notes Uploded

--- Pallavi_Hujare/Python/indexing.py
+++ Pallavi_Hujare/Python/indexing.py
@@ -9,3 +9,21 @@ l1 = [1,2,3,4,5,6]
 print(l1[2])   #3
 print(l1[-4])  #3
 print(l1[-5])  #2
+
+
+l1 = [1,2,3,[4,5,6],7]
+print(l1[-2][-1])
+# Output: 6
+
+
+name = "Pallavi"
+print(name[5])
+# Output: v
+
+l1 = [100,200,300,400,500]
+print(l1[4])
+# Output: 500
+
+l1 = [1,2,3,[4,5,6],7]
+print(l1[3][2])
+# Output: 6
