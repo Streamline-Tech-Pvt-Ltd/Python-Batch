eeba0b3bdb35ba197c0cee0aa6faea95305b3ee9
pallavihujare123-cell <pallavihujare123@gmail.com>
2026-07-22 12:21:19 +0530

    Work Done

--- Pallavi_Hujare/Python/demo.py
+++ Pallavi_Hujare/Python/demo.py
@@ -1,100 +1,84 @@
-# print("Hello World!")
+print("Hello World")
 
-# name =input("Enter Your name:")
-# print("Hello",name)
+#Sum of Two Numbers
+a = 10
+b = 20
+sum = a + b
+print("Sum is :",sum)
 
+#Print of even and odd
+num = int(input("Enter the Number :"))
+if num % 2 ==0:
+    print("Even Number ")
+else:
+    print("Odd Number ")
 
-# a=int(input("Enter The First Number:"))
-# b=int(input("Enter The Secound Number:"))
-# print("Sum is:",a+b)
+#Print Even Number
+for i in range(2,11,2):
+    print(i, end= " ")
+2
+#print odd Number 
+for i in range(1,11,2):
+    print(i, end=" ")
 
+#Print Natural Numbers
+num = int(input("Enter the Number :"))
+for num in range(1 ,num+1):
+    print(num)
 
-# num = int(input("Enter The Number:"))
-# if num % 2 == 0 :
-#     print(f"{num} is even")
-# else :
-#     print(f"{num} is odd ")
+#print factorial Number 
+num = int(input("Enter the Number :"))
+fact = 1
+for i in range(1,num+1):
+    fact = fact * i
+print(fact)
 
+#Print Reverse Number
+num =int(input("Enter Number :"))
+rev = 0
+while num > 0:
+    rev = rev * 10 + num % 10
+    num = num // 10
+print(rev)
 
-# a=int(input("Enter The First Number:"))
-# b=int(input("Enter The Secound Number:"))
-# if a>b :
-#     print("a is largest Number")
-# else:
-#     print("b is largest Number")
+#print pellindrom is not
+num =int(input("Enter Number :"))
+temp = num
+rev = 0
+while num> 0:
+    rev = rev * 10 + num % 10
+    num = num // 10
+if temp == rev:
+    print("Pellindrome")
+else:
+    print("Not Pellindrome")
 
-
-# name =input("Enter Your Name:")
-# age =int(input("Enter Your age:"))
-# city =input("Enter your city")
-
-# print(f"My Name is:{name},\nMy age is:{age},\nMy city is:{city}")
-
-
-#num = int(input("Enter the number : "))
-# fact = 1
-# for i in range(1,num+1):
-#     fact = fact * i
-# print(fact)
-
-
-# num = int(input("Enter the number of terms: "))
-# a = 0
-# b = 1
-# for i in range(num):
-#     print(a, end=" ")
-#     c = a + b
-#     a = b
-#     b = c
-
-def add_details():
-    ID =int(input("Enter Your ID:\n"))
-    Name = input("Enter Your Name :\n")
-
-    with open("sample.txt","a") as file:
-        file.write(f"Id : {ID}\n")
-        file.write(f"Name : {Name}\n")
-
-def view():
-     try:
-        with open("sample.txt","r") as file:
-            f1 = file.read()
-            if f1:
-                print(f1)
-            else:
-                print("file Record is not found")
-     except FileNotFoundError:
-        print("File dose not Exsit")
-
-def update():
-    try:
-        old = input("Enter old value :")
-        new = input("Enter Your new value :")
-        with open("sample.txt","r") as file:
-            f1 = file.read()
-            if old in f1:
-                f1 =f1.replace(old,new) 
-                with open("sample.txt","w") as file:
-                    file.write(f1)
-
-                with open("sample.txt","r") as file:
-                    print(file.read())
-            else:
-                print("Value is not found")
-    except ValueError:
-        print("Value Error")
+#Fibonacci series
+num =int(input("Enter Number :"))
+a = 0
+b = 1
+while num >0:
+    print(a, end=" ")
+    c = a + b
+    a = b
+    b = c
+    num = num - 1
 
 
-while True:
-    print("1:Add intern")
-    print("2.viwe intern")
-    print("3:Update")
-    choise =int(input("Enter Your Choise:\n"))
-    if choise == 1:
-        add_details()
-    elif choise ==2:
-         view()
-    elif choise ==3:
-        update()
-    else:
-        print("Thank You")
+#Amstrong number or not 
+num = int(input("Enter Number: "))
+temp = num
+count = 0
+sum = 0
+while temp > 0:
+    count += 1
+    temp //= 10
+temp = num
+while temp > 0:
+    sum = sum + temp % 10 ** count
+    temp //= 10
+if sum == num:
+    print("Armstrong Number")
+else:
+    print("Not Armstrong Number")
+
