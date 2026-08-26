93ad6002e7001351894de65afb1221cb3907de4f
kakadanuj <kakadanuj19@gmail.com>
2026-08-26 16:05:07 +0530

    this is my First File

--- Anuj_Kakad/Back-End/python/demo.py
+++ Anuj_Kakad/Back-End/python/demo.py
@@ -3,3 +3,9 @@ print("Hello, I am Anuj Kakad")
 a= "Anuj"
 b= "Kakad"
 print(a,b)
+print("Hello, World!")
+print("This is a sample Python script.")
+print("It demonstrates basic print statements.")
+print("You can use print to display text, variables, and more.")
+print("Feel free to modify this script and experiment with Python!")
+print("Goodbye!")
