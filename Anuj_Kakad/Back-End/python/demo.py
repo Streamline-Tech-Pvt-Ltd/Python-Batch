bab7cc0143075caa66c93598c8881648597b6e91
kakadanuj <kakadanuj19@gmail.com>
2026-09-08 11:13:10 +0530

    List Done

--- Anuj_Kakad/Back-End/python/demo.py
+++ Anuj_Kakad/Back-End/python/demo.py
@@ -1,16 +1,16 @@
-# print("Hello Streamline")
-# print("Hello, I am Anuj Kakad")
-# a= "Anuj"
-# b= "Kakad"
-# print(a,b)
-# print("Hello, World!")
-# print("This is a sample Python script.")
-# print("It demonstrates basic print statements.")
-# print("You can use print to display text, variables, and more.")
-# print("Feel free to modify this script and experiment with Python!")
-# print("Goodbye!")
-# print("Sampada kakad")
-# a= 30
-# b= 60
-# c= a*b
-# print("The product of", a, "and", b, "is:", c)
+print("Hello Streamline")
+print("Hello, I am Anuj Kakad")
+a= "Anuj"
+b= "Kakad"
+print(a,b)
+print("Hello, World!")
+print("This is a sample Python script.")
+print("It demonstrates basic print statements.")
+print("You can use print to display text, variables, and more.")
+print("Feel free to modify this script and experiment with Python!")
+print("Goodbye!")
+print("Sampada kakad")
+a= 30
+b= 60
+c= a*b
+print("The product of", a, "and", b, "is:", c)
