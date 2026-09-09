772b328b65127b34d72977e50ecd426ffb61aee6
shinde-abhishek292 <shindeabhishek292@gmail.com>
2026-09-09 14:26:47 +0530

    done1

--- Abhishek_Shinde/python/back end/demo1.py
+++ Abhishek_Shinde/python/back end/demo1.py
@@ -1,12 +1,21 @@
-# creating calculator using input function:-
+# # creating calculator using input function:-
 
-input1 = int(input("Enter First Number:-"))
-input2 = int(input("Enter Second Number:-"))
+# input1 = int(input("Enter First Number:-"))
+# input2 = int(input("Enter Second Number:-"))
 
-print()
-print("The Result of Addition is:-", input1 + input2)
-print("The Result of Subtraction is:-", input1 - input2)
-print("The Result of Multiplication is:-", input1 * input2)
-print("The Result of Division is:-", input1 / input2)
+# print()
+# print("The Result of Addition is:-", input1 + input2)
+# print("The Result of Subtraction is:-", input1 - input2)
+# print("The Result of Multiplication is:-", input1 * input2)
+# print("The Result of Division is:-", input1 / input2)
 
-abhishek
+# abhishek
+
+str ="india  is my country"
+print(str[1:8])
+print(str[2:15:2])
+print(str[::])
+print(str[::-1])
+print(str[-1])
+print(str[9][-1])
+print(str[:-1])
