976772fc07df45a6f1b7709d8aaae5cd14400e78
kakadanuj <kakadanuj19@gmail.com>
2026-09-16 16:02:41 +0530

    Conditional Statement Task Completed.

--- Anuj_Kakad/Back-End/python/Con_Stat.py
+++ Anuj_Kakad/Back-End/python/Con_Stat.py
@@ -58,14 +58,13 @@ elif temp >= 15:
 else:
     print("It's cold")
 
-
 # 3. Program for checking addmision eligibility of a student based on their marks:-
 
-take percentage and entrance marks:-
-conditions:-
-percentage >= 60 & entrance marks >= 70 :- Addmission is granted
-otherwise :- Marks are not sufficient for addmission
-percentage < 60 :- not eligible for addmission
+# take percentage and entrance marks:-
+# conditions:-
+# percentage >= 60 & entrance marks >= 70 :- Addmission is granted
+# otherwise :- Marks are not sufficient for addmission
+# percentage < 60 :- not eligible for addmission
 
 per = float(input("Enter your percentage:- "))
 entrance_marks = float(input("Enter your entrance marks:-"))
@@ -73,8 +72,61 @@ entrance_marks = float(input("Enter your entrance marks:-"))
 
 if per >= 60 and entrance_marks >= 70:
     print("Addmisson is granted")
-elif per >= 60 and entrance_marks <= 70:
+elif per >= 60 and entrance_marks < 70:
     print("Marks are not sufficient for addmission")
 else:
     print("Not eligible for addmission")
 
+# 4. Program for login system:-
+
+# take username and password from user
+# conditions:-
+# username is correct and password is correct :- login successful
+# otherwise :- invalid username
+# otherwise :- invalid password
+
+username = input("Enter your username:- ")
+password = input("Enter your password:-")
+
+if username == "Admin" and password == "Pass@123":
+    print("Login Successful.")
+elif username != "Admin" and password == "Pass@123":
+    print("Invalid Usename.")
+elif username == "Admin" and password != "Pass@123":
+    print("Invalid Password.")
+else:
+    print("Invalid Username and Password")
+
+# 5. Printing Maximum of three numbers:-
+
+# take 3 numbers from user
+# conditions:
+# compare them with each other and print the largest number using conditional statements 
+
+print("Provide 3 Numbers To compare:-")
+num1 = float(input("Enter First Number:-"))
+num2 = float(input("Enter Second Number:-"))
+num3 = float(input("Enter Third Number:-"))
+
+if num1 > num2 and num1 > num3:
+    print("The Number",num1,"Is Greater")
+elif num2 > num1 and num2 > num3:
+    print("The Number",num2,"Is Greater")
+else:
+    print("The Number",num3,"Is Greater")
+
+# 6. Program To check the no. is Even or Odd:-
+
+# take a no. from user
+# conditions:-
+# if no is odd :- The no. is odd
+# if the no is even :- the no. is Even
+
+print("Provide a Number To Check:-")
+
+num = int(input("Enter a number:-"))
+
+if num % 2 == 0:
+    print("The Number Is Even.")
+else:
+    print("The Number Is Odd.")
