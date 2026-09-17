a53b948eda75247bbf50bcf72fa6f1eb80163ffd
mayuri-jadahv2004 <mayurijadhav3278gmail.com>
2026-09-17 15:32:54 +0530

    task

--- Mayuri_Jadhav/loop.task.py
+++ Mayuri_Jadhav/loop.task.py
@@ -1,32 +1,71 @@
-# #Sum of first n natural numbers using for loop
-n = int(input("Enter a number: "))
+# # #Sum of first n natural numbers using for loop
+# n = int(input("Enter a number: "))
 
-sum = 0
+# sum = 0
 
-for i in range(1, n + 1):
-    sum = sum + i
+# for i in range(1, n + 1):
+#     sum = sum + i
 
-print("Sum =", sum)
-
-
-
-
-
-# # Factorial of a number
-n = int(input("Enter a number: "))
-
-fact = 1
-
-for i in range(1, n + 1):
-    fact = fact * i
-
-print("Factorial =", fact)
+# print("Sum =", sum)
 
 
 
 
-# #Multiplication table up to 10
-n = int(input("Enter a number: "))
 
-for i in range(1, 11):
-    print(n, "x", i, "=", n * i)
+# # # Factorial of a number
+# n = int(input("Enter a number: "))
+
+# fact = 1
+
+# for i in range(1, n + 1):
+#     fact = fact * i
+
+# print("Factorial =", fact)
+
+
+
+
+# # #Multiplication table up to 10
+# n = int(input("Enter a number: "))
+
+# for i in range(1, 11):
+#     print(n, "x", i, "=", n * i)
+
+# name = "Mayuri Jadhav"
+# for i in name:
+#     print(i)
+
+# for i in range(1,11,2):
+#     print(i)
+
+# for i in range(8):
+#   print(i)
+# else:
+#      print("loop Finished")
+
+
+
+# for i in range(2,21,2):
+#     print(i)
+
+# for i in range(1,11):
+#     print(3*i)
+
+# i=1
+#  while i<=10:
+#     print(2*i)
+#     i+=1
+# num= int(input("enter a number:"))
+# i=1
+# while i<=10:
+#     print(num*i)
+#     i+=1
+
+
+# num=int(input("enput a number:"))
+# fact =1
+# for i in range(1,num+1):
+#    fact=fact*i
+#    print("factorial=",fact)
+
+
