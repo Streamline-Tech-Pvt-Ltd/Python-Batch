177652567da462755f09fe98b994de8ae1a6fa88
kakadanuj <kakadanuj19@gmail.com>
2026-09-23 23:43:50 +0530

    Ex On Function Done

--- Anuj_Kakad/Back-End/python/Functions.py
+++ Anuj_Kakad/Back-End/python/Functions.py
@@ -1,3 +1,35 @@
+# 1. print Arithematic operators using fun
+
+def add(a,b):
+    print(a+b)
+add(10,20)
+
+def sub(a,b):
+    print(a - b)
+sub(30,20)
+
+def mul(a,b):
+    print(a * b)
+mul(30,60)
+
+def div(a,b):
+    print(a / b)
+div(20,2)
+
+# 2. print a number is natural or not using a fun
+
+def check_natural(num):
+    if num >= 1:
+        print("number is natural.")
+    else:
+        print("number is not natural.")
+
+number = input("Enter a no:- ")
+
+check_natural(number)
+
+
+
 # Example using user defined function:-
 
 def check_number(num):
