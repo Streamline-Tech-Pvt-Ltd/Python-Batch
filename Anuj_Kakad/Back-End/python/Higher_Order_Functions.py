bcbe7a1801a62bd6394bbc0b9e64140056100421
kakadanuj <kakadanuj19@gmail.com>
2026-09-24 13:31:39 +0530

    Ex on Higher Order Functions Done.

--- Anuj_Kakad/Back-End/python/Higher_Order_Functions.py
+++ Anuj_Kakad/Back-End/python/Higher_Order_Functions.py
@@ -1,18 +1,18 @@
-# # Examples Of map() Higher Order function:-
+# Examples Of map() Higher Order function:-
 
-# # Example 1: Square of numbers
+# Example 1: Square of numbers
 
-# numbers = [1, 2, 3, 4, 5]
-# result = list(map(lambda x: x * x, numbers))
-# print(result)
+numbers = [1, 2, 3, 4, 5]
+result = list(map(lambda x: x * x, numbers))
+print(result)
 
 
-# print()
-# # Example 2: Double the numbers
+print()
+# Example 2: Double the numbers
 
-# numbers = [10, 20, 30, 40]
-# result = list(map(lambda x: x * 2, numbers))
-# print(result)
+numbers = [10, 20, 30, 40]
+result = list(map(lambda x: x * 2, numbers))
+print(result)
 
 # numbers1 = [10, 20, 30, 40]
 
@@ -21,88 +21,88 @@
 # print(res)
 
 
-# print()
-# # Example 3: Convert names to uppercase
+print()
+# Example 3: Convert names to uppercase
 
-# names = ["anuj", "rahul", "amit"]
-# result = list(map(lambda x: x.upper(), names))
-# print(result)
-
-
-# print()
-# # Examples on filter() Higher order Function:-
-
-# # Example 1: Filter even numbers
-
-# numbers = [1, 2, 3, 4, 5, 6]
-# result = list(filter(lambda x: x % 2 == 0, numbers))
-# print(result)
+names = ["anuj", "rahul", "amit"]
+result = list(map(lambda x: x.upper(), names))
+print(result)
 
 
-# print()
-# # Example 2: Find numbers greater than 10
+print()
+# Examples on filter() Higher order Function:-
 
-# numbers = [5, 12, 8, 20, 15]
-# result = list(filter(lambda x: x > 10, numbers))
-# print(result)
+# Example 1: Filter even numbers
 
-
-# print()
-# # Example 3: Filter names starting with "A"
-
-# names = ["Anuj", "Rahul", "Amit", "Sneha"]
-# result = list(filter(lambda x: x.startswith("A"), names))
-# print(result)
+numbers = [1, 2, 3, 4, 5, 6]
+result = list(filter(lambda x: x % 2 == 0, numbers))
+print(result)
 
 
-# print()
-# # Examples on reduce() higher Order Function:-
+print()
+# Example 2: Find numbers greater than 10
 
-# # Example 1: Sum of numbers
-
-# from functools import reduce
-
-# numbers = [1, 2, 3, 4, 5]
-# result = reduce(lambda x, y: x + y, numbers)
-# print(result)
+numbers = [5, 12, 8, 20, 15]
+result = list(filter(lambda x: x > 10, numbers))
+print(result)
 
 
-# print()
-# # Example 2: Product of numbers
+print()
+# Example 3: Filter names starting with "A"
 
-# from functools import reduce
-
-# numbers = [1, 2, 3, 4, 5]
-# result = reduce(lambda x, y: x * y, numbers)
-# print(result)
+names = ["Anuj", "Rahul", "Amit", "Sneha"]
+result = list(filter(lambda x: x.startswith("A"), names))
+print(result)
 
 
-# print()
-# # Example 3: Find the largest number
+print()
+# Examples on reduce() higher Order Function:-
 
-# from functools import reduce
+# Example 1: Sum of numbers
 
-# numbers = [10, 25, 8, 40, 15]
-# result = reduce(lambda x, y: x if x > y else y, numbers)
-# print(result)
+from functools import reduce
 
-
-# # 4 
-# list3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# res = 0
-
-# for num in list3:
-#     res = res + num
-
-# print(res)
+numbers = [1, 2, 3, 4, 5]
+result = reduce(lambda x, y: x + y, numbers)
+print(result)
 
 
-# from functools import reduce
+print()
+# Example 2: Product of numbers
 
-# list3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15]
-# res = reduce(lambda a, b: a + b, list3)
+from functools import reduce
 
-# print(res)
+numbers = [1, 2, 3, 4, 5]
+result = reduce(lambda x, y: x * y, numbers)
+print(result)
+
+
+print()
+# Example 3: Find the largest number
+
+from functools import reduce
+
+numbers = [10, 25, 8, 40, 15]
+result = reduce(lambda x, y: x if x > y else y, numbers)
+print(result)
+
+
+# 4 
+list3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+res = 0
+
+for num in list3:
+    res = res + num
+
+print(res)
+
+
+from functools import reduce
+
+list3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15]
+res = reduce(lambda a, b: a + b, list3)
+
+print(res)
 
 
 # User Defined Higher Order Function:-
@@ -122,7 +122,11 @@ def mul(e,f):
 def div(g,h):
     return g/h
 
+def mod(i,j):
+    return i%j
+
 print(calculate1(add,10,20))
 print(calculate1(sub,20,10))
 print(calculate1(mul,10,20))
 print(calculate1(div,20,2))
+print(calculate1(mod,10,20))
